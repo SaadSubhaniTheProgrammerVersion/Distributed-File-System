35867002af1e984d084f3364b8cd8b07c1a140bb
Huzaiifaaaa <huzaifah062@gmail.com>
2023-05-15 17:58:37 +0500

    Try Catch

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 
 app = Flask(__name__)
 
+
+STORAGE_DIRECTORY=r'C:\Users\huzai\Documents\GitHub\Distributed-File-System\static\files'
 apitoken="shahkhalid"
 
 @app.route("/")
@@ -14,78 +16,90 @@ def Index():
 
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST':
-        token = request.args.get("token")
-        filepath = request.args.get("filepath")
-        file = request.files["file"]
+    try:
+        if request.method == 'POST':
+            token = request.args.get("token")
+            filepath = request.args.get("filepath")
+            file = request.files["file"]
 
-        if file  and token==apitoken:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(r'C:\Users\huzai\Documents\GitHub\Distributed-File-System\static\files', filename))
-            return "File uploaded!"
+            if file  and token==apitoken:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(STORAGE_DIRECTORY, filename))
+                return "File uploaded!"
+            else:
+                return "Invalid Token!"
         else:
-            return "Invalid Token!"
+            return "Error!"
+    except:
+        return "Error!"
 
 
 
 @app.route('/download', methods=['GET', 'POST'])
 def download():
-    token = request.args.get("token")
-    filepath = request.args.get("filepath")
+    try:
+        token = request.args.get("token")
+        filepath = request.args.get("filepath")
 
-    if token==apitoken:
-        filepath=os.path.join(r'C:\Users\huzai\Documents\GitHub\Distributed-File-System\static\files', filepath)
-        if os.path.isfile(filepath):
-            return send_file(filepath, as_attachment=True)
+        if token==apitoken:
+            filepath=os.path.join(STORAGE_DIRECTORY, filepath)
+            if os.path.isfile(filepath):
+                return send_file(filepath, as_attachment=True)
+            else:
+                return "File not found!"
         else:
-            return "File not found!"
-    else:
-        return "Invalid Token!"
+            return "Invalid Token!"
+    except:
+        return "Error!"
 
 @app.route('/listAllFiles', methods=['GET', 'POST'])
 def listAllFiles():
-    token = request.args.get("token")
+    try:
+        token = request.args.get("token")
 
-    if token==apitoken:
-        directory=r'C:\Users\huzai\Documents\GitHub\Distributed-File-System\static\files'
-        files=[]
+        if token==apitoken:
+            directory=STORAGE_DIRECTORY
+            files=[]
 
-        for filename in os.listdir(directory):
-            filepath=os.path.join(directory, filename)
-            
-            file_info = {
-                "name": filename,
-                'path': filepath,
-            }
-            files.append(file_info)
+            for filename in os.listdir(directory):
+                filepath=os.path.join(directory, filename)
 
-        return jsonify(files)
-    else:
-        return "Invalid Token!"
-
-
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    token = request.args.get("token")
-    query=request.args.get("query")
-
-    if token==apitoken:
-        directory=r'C:\Users\huzai\Documents\GitHub\Distributed-File-System\static\files'
-        files=[]
-
-        for filename in os.listdir(directory):
-            filepath=os.path.join(directory, filename)
-            if query in filename:
                 file_info = {
                     "name": filename,
                     'path': filepath,
                 }
                 files.append(file_info)
 
-        return jsonify(files)
-    else:
-        return "Invalid Token!"
-    return
+            return jsonify(files)
+        else:
+            return "Invalid Token!"
+    except:
+        return "Error!"
 
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    try:
+        token = request.args.get("token")
+        query=request.args.get("query")
+
+        if token==apitoken:
+            directory=STORAGE_DIRECTORY
+            files=[]
+
+            for filename in os.listdir(directory):
+                filepath=os.path.join(directory, filename)
+                if query in filename:
+                    file_info = {
+                        "name": filename,
+                        'path': filepath,
+                    }
+                    files.append(file_info)
+
+            return jsonify(files)
+        else:
+            return "Invalid Token!"
+    except:
+        return "Error!"
 
 app.run()
