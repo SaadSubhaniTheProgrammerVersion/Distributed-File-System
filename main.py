e78e473b903bd0096c904da4fb4fc1ffedb6067f
Huzaiifaaaa <huzaifah062@gmail.com>
2023-05-15 15:16:54 +0500

    Basic Code

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Distributed File System")
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+import os
+import requests
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def Index():
+    return render_template("Index.html")
+
+
+@app.route('/upload', methods = ['POST'])
+def upload():
+    return
+
+@app.route('/download', methods = ['GET'])
+def download():
+    return
+
+def search():
+    return
+
+app.run()
